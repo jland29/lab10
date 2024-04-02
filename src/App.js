ef3c44606f47e71d22939ddce2561bc8e240b23e
jland29 <143559640+jland29@users.noreply.github.com>
2024-04-02 04:50:13 +0000

    Test update

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React with John in MI449
         </a>
       </header>
     </div>
