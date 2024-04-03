3863e70f87687a556945c92ece320513a07bcb18
jland29 <143559640+jland29@users.noreply.github.com>
2024-04-03 21:13:01 +0000

    Lab complete

--- src/App.js
+++ src/App.js
@@ -1,7 +1,64 @@
+import {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+function DisplayCourse() {
+  // conditional
+  const [enrolled, setEnroll] = useState(false);
+  // data
+  const [courseInfo, setInfo] = useState(
+    {
+      name: 'Intro to Web Development',
+      instructor: 'Instructor 1',
+      semester: 'Spring',
+      year: 2024,
+      enrolled: 48,
+      capacity: 50
+    }
+  );
+
+  // hook
+  function enroll() {
+    setEnroll(!enrolled);
+    
+    if (enrolled) {
+      setInfo({
+        ...courseInfo,
+        enrolled: courseInfo.enrolled - 1
+      });
+    }
+    else {
+      setInfo({
+        ...courseInfo,
+        enrolled: courseInfo.enrolled + 1
+      });
+    }
+  }
+
+  // return 2 components: data and enroll button
+  return (
+    <div>
+      <h1>{courseInfo.name}</h1>
+      <h2>{courseInfo.instructor}</h2>
+      <h3>{courseInfo.semester} {course.year}</h3>
+      <h4>Capacity: {courseInfo.enrolled} / {courseInfo.capacity}</h4>
+
+      <button onClick={enroll}>
+        {enrolled ? 'Remove' : 'Enroll in'} this course
+      </button>
+    </div>
+  );
+}
+
+// 3rd component is login button
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  function login() {
+    setLoggedIn(!loggedIn);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -15,8 +72,15 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with John in MI449
+          Learn React with John in MI449 Lab 10
         </a>
+        <div>
+          
+        </div>
+        <button onClick={login}>
+          Log {loggedIn ? 'out' : 'in'}
+        </button>
+        {loggedIn && <DisplayCourse />}
       </header>
     </div>
   );
